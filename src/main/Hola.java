d731f66fdccfa47f21fd964b8b45f07aab7a5a23
PtakDev <79053918+PtakDev@users.noreply.github.com>
2022-04-18 14:40:41 +0200

    cuatro commit

--- src/main/Hola.java
+++ src/main/Hola.java
@@ -3,7 +3,7 @@ package main;
 public class Hola {
 	
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo de Espana");
+		System.out.println("Hola Mundo de Espana!");
 
 	}
 
